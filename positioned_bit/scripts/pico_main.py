463169862b883624a21bddbb777d4e6cd967575b
eliasab16 <55062776+eliasab16@users.noreply.github.com>
2026-03-17 23:06:16 -0700

    pico main script

--- positioned_bit/scripts/pico_main.py
+++ positioned_bit/scripts/pico_main.py
@@ -1,14 +1,27 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, I2C
 import sys
 import select
-from time import sleep_ms, ticks_us, ticks_diff
+from time import ticks_us, ticks_diff
 
 # This script should be running on the pico. It's used to control the motors.
 
-# TMC2209 VDD power via GP7
-vdd = Pin(7, Pin.OUT)
-vdd.value(1)
-sleep_ms(50)
+# Laser on GP7 (via PN2222 transistor, 1k base resistor)
+laser = Pin(7, Pin.OUT)
+laser.value(0)
+
+# ToF sensor VL53L4CD on I2C0 (GP8=SDA, GP9=SCL)
+i2c0 = I2C(0, sda=Pin(8), scl=Pin(9), freq=400_000)
+tof_ok = False
+try:
+    from vl53l4cd import VL53L4CD
+    tof = VL53L4CD(i2c0)
+    tof.inter_measurement = 0
+    tof.timing_budget = 20
+    tof.start_ranging()
+    tof_ok = True
+    print("VL53L4CD ready")
+except Exception as e:
+    print("VL53L4CD error:", e)
 
 # Bit motor: DC worm gear via DRV8871
 in1 = PWM(Pin(2))
@@ -37,6 +50,9 @@ z_moving = 0
 z_last = ticks_us()
 z_state = 0
 
+# ToF cached distance
+tof_dist = -1
+
 # Limit switches (active LOW with pull-up)
 y_lower_limit = Pin(16, Pin.IN, Pin.PULL_UP)
 z_back_limit  = Pin(17, Pin.IN, Pin.PULL_UP)
@@ -50,7 +66,7 @@ while True:
     now = ticks_us()
 
     if poll.poll(0):
-        cmd = sys.stdin.readline().strip()
+        cmd = sys.stdin.read(1)
         if cmd == "1":
             in1.duty_u16(40000)
             in2.duty_u16(0)
@@ -79,6 +95,14 @@ while True:
         elif cmd == "6":
             z_moving = 0
             z_en.value(1)
+        elif cmd == "l":
+            laser.value(1 - laser.value())
+            print("LASER", "ON" if laser.value() else "OFF")
+        elif cmd == "d":
+            if tof_ok:
+                print("D", tof_dist)
+            else:
+                print("TOF NOT READY")
     
     # check the limit switches for each axis and stop when triggered 
     if y_dir == 1 and y_upper_limit.value() == 0:
@@ -114,3 +138,8 @@ while True:
         z_state = 1 - z_state
         z_step.value(z_state)
         z_last = now
+
+    # Non-blocking ToF read: grab latest distance if data is ready
+    if tof_ok and tof.data_ready:
+        tof_dist = tof.distance
+        tof.clear_interrupt()
